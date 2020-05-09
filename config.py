fae1d0d28a6ee635e4a12f82f5027f7c3f7e9990
JedCDephy <jcummings@dephy.com>
2020-05-09 18:37:20 -0400

    chapter 5

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECTER_KEY') or 'you-will-never-guess'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
